654d6c42ea9b7537cbb987674c39ef4bf5739466
Mike Silva <mike.a.silva@gmail.com>
2025-11-14 11:32:29 -0500

    Minor format changes

--- robot.py
+++ robot.py
@@ -8,17 +8,18 @@ import xrp
 os.environ["HALSIMXRP_HOST"] = "192.168.42.1"
 os.environ["HALSIMXRP_PORT"] = "3540"
 
+
 class MyRobot(magicbot.MagicRobot):
     controller: XboxController
     led: LED
 
     def createObjects(self):
-        '''Create motors and stuff here'''
+        """Create motors and stuff here"""
         self.on_board_led = xrp.XRPOnBoardIO()
         self.controller_port = 0
 
     def teleopInit(self):
-        '''Called when teleop starts; optional'''
+        """Called when teleop starts; optional"""
         self.led.on()
 
     def teleopPeriodic(self):
