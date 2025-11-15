639ea41d256b709c6d63e39456ada91a29ce34a3
Mike Silva <mike.a.silva@gmail.com>
2025-11-14 20:15:15 -0500

    Initial commit of challenge 4 solution

---
+++ autonomous/driveforward.py
@@ -0,0 +1,15 @@
+from magicbot import AutonomousStateMachine, timed_state
+from components.drivetrain import Drivetrain
+
+
+class DriveForward(AutonomousStateMachine):
+
+    MODE_NAME = "Drive Forward"
+    DEFAULT = True
+
+    # Injected from the definition in robot.py
+    drivetrain: Drivetrain
+
+    @timed_state(duration=3, first=True)
+    def drive_forward(self):
+        self.drivetrain.go(-0.7, 0)
