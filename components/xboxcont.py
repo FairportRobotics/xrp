1899b65ad11710a22dde538f49d8a55844b711f3
vaishu <vaishnavi.r.das@gmail.com>
2025-11-20 19:46:48 -0500

    stuff

---
+++ components/xboxcont.py
@@ -0,0 +1,31 @@
+import wpilib
+
+
+class XboxController:
+    port: int
+
+    def execute(self):
+        pass
+
+    def setup(self):
+        self.controller = wpilib.XboxController(self.port)
+
+    def joysticks(self):
+        return (
+            self.controller.getLeftX(),
+            self.controller.getLeftY(),
+            self.controller.getRightX(),
+            self.controller.getRightY(),
+        )
+
+    def x_pressed(self):
+        return self.controller.getXButton()
+
+    def y_pressed(self):
+        return self.controller.getYButton()
+    
+    def a_pressed(self):
+        return self.controller.getAButton()
+
+    def b_pressed(self):
+        return self.controller.getButton()
--- robot.py
+++ robot.py
@@ -1,1 +1,29 @@
-# TODO: insert robot code here
+import magicbot
+import wpilib
+from components.led import LED
+from components.xboxcont import XboxController
+import xrp 
+import os
+
+os.environ["HALSIMXRP_HOST"] = "192.168.42.1"
+os.environ["HALSIMXRP_PORT"] = "3540"
+
+class MyRobot(magicbot.MagicRobot):
+    led: LED 
+    controller: XboxController
+
+    def createObjects(self):
+        '''Create motors and stuff here'''
+        self.on_board_led = xrp.XRPOnBoardIO()
+        self.port = 0
+
+    def teleopInit(self):
+        '''Called when teleop starts; optional'''
+        self.led.on()
+
+    def teleopPeriodic(self): 
+        pass
+    
+    
+    
+    
