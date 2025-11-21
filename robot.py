e44271247dff12ac6ca7d378b914981c4c0e47e0
Nanson Chen <buyingsaucer2@gmail.com>
2025-11-20 19:53:22 -0500

    somemessage

--- robot.py
+++ robot.py
@@ -1,1 +1,16 @@
 # TODO: insert robot code here
+import magicbot
+import wpilib
+
+class MyRobot(magicbot.MagicRobot):
+
+    def createObjects(self):
+        '''Create motors and stuff here'''
+        pass
+
+    def teleopInit(self):
+        '''Called when teleop starts; optional'''
+        pass
+    
+    def teleopPeriodic(self):
+        pass
